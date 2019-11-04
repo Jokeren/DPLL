99081f7df3956400a7c8648c98f1ce25ea2cc4e1
Jokeren <robinho364@gmail.com>
2019-11-04 15:10:30 -0600

    Add exception handling

--- tiny_sat.cc
+++ tiny_sat.cc
@@ -21,23 +21,30 @@ int main(int argc, char *argv[]) {
   cxxopts::Options options("tinySAT", "minimal SAT solver implementation");
   options.add_options()
     ("i,input", "Input path of a DIMACS file", cxxopts::value<std::string>())
-    ("s,solver", "Choose a SAT solver (default tiny):\nRandom solver (random)\nTwo clause solver (two)\nTiny solver (tine)n",
+    ("s,solver", "Choose a SAT solver: random, two, tiny",
      cxxopts::value<std::string>()->default_value("tiny"))
     ("h,help", "tinySAT help");
 
   if (argc == 1) {
+    TINY_SAT_LOG_INFO("Help", options.help().c_str());
     return 0;
   }
 
   auto result = options.parse(argc, argv);
 
   if (result["help"].count() || result["h"].count()) {
+    TINY_SAT_LOG_INFO("Help", options.help().c_str());
     return 0;
   }
 
-  std::string input = result["i"].count() ? result["i"].as<std::string>() : result["input"].as<std::string>();
-  std::string solver_name = result["s"].count() ? result["s"].as<std::string>() : result["solver"].as<std::string>();
+  std::string input = result["i"].count() ? result["i"].as<std::string>() :
+    (result["input"].count() ?  result["input"].as<std::string>() : "");
+  if (input.size() == 0) {
+    TINY_SAT_LOG_INFO("Help", options.help().c_str());
+    return 0;
+  }
 
+  std::string solver_name = result["s"].count() ? result["s"].as<std::string>() : result["solver"].as<std::string>();
   tiny_sat::DIMACS dimacs;
   if (dimacs.open(input)) {
     tiny_sat::CNF cnf;
