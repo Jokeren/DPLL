524e4e3be4645fe28d9119543d4af000d9e798eb
Jokeren <robinho364@gmail.com>
2019-11-06 15:13:02 -0600

    Add data

---
+++ scripts/gen_data.py
@@ -0,0 +1,17 @@
+#!/bin/python
+
+import os
+
+P=[100,125,150,175,200,225,250]
+
+for p in P:
+    prefix = "extern/data/p" + str(p)
+    os.system("mkdir -p " + prefix)
+    r = 3.0
+    while r < 6.0:
+        c = int(r * p)
+        r += 0.2
+        output = prefix + "/p" + str(p) + "c" + str(c) + "l3.cnf"
+        cmd = "./tiny_sat -o " + output + " -p " + str(p) + " -c " + str(c) + " -l 3"
+        print(cmd)
+        os.system(cmd)
