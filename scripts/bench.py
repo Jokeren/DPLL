75bf3a59649d37e3a5cd3a4b5ee312f8f39c5604
Jokeren <robinho364@gmail.com>
2019-11-30 16:30:57 -0600

    Add benchmark script

---
+++ scripts/bench.py
@@ -0,0 +1,35 @@
+#!/bin/python
+
+import subprocess, threading
+
+class Command(object):
+    def __init__(self):
+        self.process = None
+
+    def run(self, cmd, timeout):
+        def target():
+            self.process = subprocess.Popen(cmd, shell=True)
+            self.process.communicate()
+
+        thread = threading.Thread(target=target)
+        thread.start()
+
+        thread.join(timeout)
+        if thread.is_alive():
+            self.process.terminate()
+            thread.join()
+
+N = [100, 150, 200]
+S = ["random", "two", "tiny"]
+command = Command()
+
+for n in N:
+    for s in S:
+        R = 3.0
+        while R <= 6.0:
+            L = int(R * n)
+            P = "p" + str(n) + "c" + str(L) + "l3.cnf"
+            sh = "./tiny_sat -i extern/data/" + "p" + str(n) + "/" + P + " -s " + s
+            print sh
+            command.run(sh, timeout=60);
+            R += 0.2
